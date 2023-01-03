382c915aa9582b530b19f4c944fc9f99fcef452b
VANSHTYAGI70 <119074943+VANSHTYAGI70@users.noreply.github.com>
2023-01-03 11:44:59 +0530

    Update class42-If-elsestatementinPython-CipherSchools.py

--- class42-If-elsestatementinPython-CipherSchools.py
+++ class42-If-elsestatementinPython-CipherSchools.py
@@ -1,6 +1,6 @@
 age = int(input("enter your age : "))
 if age >= 14:
     print("line a ")
-    print("you are above 14")
+    print("you are above 14 you can play")
 else:
     print("sorry, you can't play ")
